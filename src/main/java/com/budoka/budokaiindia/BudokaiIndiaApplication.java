1db2e4e12de200860cec6dd68153d4c4bd675b7f
MUKESH DANGI <mukesh@guest-wireless-upc-1602-10-120-045-095.usc.edu>
2018-05-22 14:19:36 +0530

    updates

--- src/main/java/com/budoka/budokaiindia/BudokaiIndiaApplication.java
+++ src/main/java/com/budoka/budokaiindia/BudokaiIndiaApplication.java
@@ -1,12 +1,15 @@
 package com.budoka.budokaiindia;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BudokaiIndiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BudokaiIndiaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.error("Hello World");
+        SpringApplication.run(BudokaiIndiaApplication.class, args);
+    }
 }

fe55216fc7f6e076c45b692ce6bd102be9071b19
MUKESH DANGI <mukesh@guest-wireless-upc-1602-10-120-045-095.usc.edu>
2018-05-22 13:06:46 +0530

    budo kai india start

---
+++ src/main/java/com/budoka/budokaiindia/BudokaiIndiaApplication.java
@@ -0,0 +1,12 @@
+package com.budoka.budokaiindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudokaiIndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudokaiIndiaApplication.class, args);
+	}
+}
